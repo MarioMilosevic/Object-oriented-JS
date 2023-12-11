86a07baa1291e149c2086ea23166034585d09c96
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-11 19:19:25 +0100

    ES 6classes

--- script.js
+++ script.js
@@ -1,94 +1,182 @@
-"use strict";
+// "use strict";
 
-// Properti instance
+// // Properti instance
 // const Person = function (firstName, birthYear) {
 //   this.firstName = firstName;
 //   this.birthYear = birthYear;
 
-// Nikada ovo ne raditi !!!!!!!!!!
-//   this.calcAge = function(){
-//     console.log(2023 - this.birthYear);
-//   }
+//   // Nikada ovo ne raditi !!!!!!!!!!
+//   //   this.calcAge = function(){
+//   //     console.log(2023 - this.birthYear);
+//   //   }
 // };
 
-// const mario = new Person("Mario", 1996);
-// console.log(mario);
+// Person.hey = function () {
+//   console.log("Hey there my nigga");
+// };
 
-// // 1. Novi {} je napravljen
-// // 2. funkcija je pozvana this = napravljeni objekat
-// // 3. {} je povezan za prototipom
-// // 4. funkcija automatski vraca {}
+// Person.hey();
+// // const mario = new Person("Mario", 1996);
+// // console.log(mario);
 
-// const milica = new Person("Milica", 1988);
-// const toni = new Person("Toni", 1994);
-// console.log(milica,toni);
+// // // 1. Novi {} je napravljen
+// // // 2. funkcija je pozvana this = napravljeni objekat
+// // // 3. {} je povezan za prototipom
+// // // 4. funkcija automatski vraca {}
 
-// console.log(mario instanceof Person)
+// // const milica = new Person("Milica", 1988);
+// // const toni = new Person("Toni", 1994);
+// // console.log(milica,toni);
 
-// // Prototipi
-// console.log(Person.prototype);
-// Person.prototype.calcAge = function(){
-//     console.log(2023 - this.birthYear);
+// // console.log(mario instanceof Person)
+
+// // // Prototipi
+// // console.log(Person.prototype);
+// // Person.prototype.calcAge = function(){
+// //     console.log(2023 - this.birthYear);
+// //   }
+
+// // mario.calcAge()
+// // milica.calcAge()
+// // toni.calcAge()
+// // console.log(mario.__proto__);
+// // console.log(mario.__proto__ === Person.prototype);
+
+// // console.log(Person.prototype.isPrototypeOf(mario));
+// // console.log(Person.prototype.isPrototypeOf(milica));
+// // console.log(Person.prototype.isPrototypeOf(toni));
+
+// // console.log(Person.prototype.isPrototypeOf(Person));
+
+// // Person.prototype.species = 'Homo Sapiens'
+// // console.log(mario,toni);
+
+// // console.log(mario.hasOwnProperty('firstName'));
+// // console.log(mario.hasOwnProperty('species'));
+
+// // console.log(mario.__proto__.__proto__.__proto__);
+
+// // const arr = [3,5,1,4,87,464,13,4,1,31,46,34,3,34]
+// // console.log(arr.__proto__);
+
+// // Coding Challenge #1
+
+// /* 
+// 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+// 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+// DATA CAR 1: 'BMW' going at 120 km/h
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+
+// GOOD LUCK 😀
+// */
+
+// // const Car = function (make, speed) {
+// //   this.make = make;
+// //   this.speed = speed;
+// // };
+
+// // Car.prototype.accelerate = function () {
+// //   console.log(this.speed += 10);
+// // };
+
+// // Car.prototype.brake = function () {
+// //   console.log(this.speed -= 5);
+// // };
+
+// // const bmw = new Car('BMW',120)
+// // const mercedes = new Car('Mercedes',95)
+
+// // bmw.accelerate()
+// // bmw.accelerate()
+// // bmw.accelerate()
+// // bmw.brake()
+
+// // mercedes.accelerate()
+// // mercedes.accelerate()
+// // mercedes.brake()
+
+// // class expression
+// // const PersonCl = class {
+
+// // }
+
+// //class declaration
+
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
 //   }
 
-// mario.calcAge()
-// milica.calcAge()
-// toni.calcAge()
-// console.log(mario.__proto__);
-// console.log(mario.__proto__ === Person.prototype);
+//   calcAge() {
+//     console.log(2023 - this.birthYear);
+//   }
+//   greet() {
+//     console.log(`Hey ${this.firstName}`);
+//   }
 
-// console.log(Person.prototype.isPrototypeOf(mario));
-// console.log(Person.prototype.isPrototypeOf(milica));
-// console.log(Person.prototype.isPrototypeOf(toni));
+//   get age() {
+//     return 2023 - this.birthYear;
+//   }
 
-// console.log(Person.prototype.isPrototypeOf(Person));
+//   set fullName(name) {
+//     if (name.includes(" ")) this._fullName = name;
+//     else alert("No last name");
+//   }
 
-// Person.prototype.species = 'Homo Sapiens'
-// console.log(mario,toni);
+//   static hey() {
+//     console.log("Hey there my nigga");
+//   }
+// }
 
-// console.log(mario.hasOwnProperty('firstName'));
-// console.log(mario.hasOwnProperty('species'));
+// const mario = new PersonCl("mario milosevic", 1996);
+// console.log(mario);
+// mario.calcAge();
+// mario.greet();
+// // 1. Classes are not hoisted
+// // 2. Classes are first-class citizens
+// // 3. Classes are executed in strict-mode
 
-// console.log(mario.__proto__.__proto__.__proto__);
+// const account = {
+//   owner: "mario",
+//   movements: [2000, 300, 4050, 540],
 
-// const arr = [3,5,1,4,87,464,13,4,1,31,46,34,3,34]
-// console.log(arr.__proto__);
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
 
-// Coding Challenge #1
+// console.log(account.latest);
+// account.set = 50;
+// console.log(account.movements);
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2023 - this.birthYear);
+//   },
+// };
+
+// const mario2 = Object.create(PersonProto);
+// mario2.name = "Mario"
+// mario2.birthYear = 1996
+// mario2.calcAge()
+// console.log(mario2);
+///////////////////////////////////////
+// Coding Challenge #2
 
 /* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
 
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
+DATA CAR 1: 'Ford' going at 120 km/h
 
 GOOD LUCK 😀
 */
-
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-
-Car.prototype.accelerate = function () {
-  console.log(this.speed += 10);
-};
-
-Car.prototype.brake = function () {
-  console.log(this.speed -= 5);
-};
-
-const bmw = new Car('BMW',120)
-const mercedes = new Car('Mercedes',95)
-
-bmw.accelerate()
-bmw.accelerate()
-bmw.accelerate()
-bmw.brake()
-
-mercedes.accelerate()
-mercedes.accelerate()
-mercedes.brake()
