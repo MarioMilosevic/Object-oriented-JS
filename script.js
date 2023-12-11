520facccda5bbdba24ef8dddb53291dbc2b5bd05
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-11 19:55:43 +0100

    Finish second exercise

--- script.js
+++ script.js
@@ -61,7 +61,7 @@
 
 // // Coding Challenge #1
 
-// /* 
+// /*
 // 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
 // 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 // 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
@@ -180,3 +180,52 @@ DATA CAR 1: 'Ford' going at 120 km/h
 
 GOOD LUCK 😀
 */
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+   this.speed = speed *1.6
+  }
+}
+
+const ford = new Car("Ford", 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+console.log( ford.speedUS = 50)
+
+// // const Car = function (make, speed) {
+// //   this.make = make;
+// //   this.speed = speed;
+// // };
+
+// // Car.prototype.accelerate = function () {
+// //   console.log(this.speed += 10);
+// // };
+
+// // Car.prototype.brake = function () {
+// //   console.log(this.speed -= 5);
+// // };
+
+// // const bmw = new Car('BMW',120)
+// // const mercedes = new Car('Mercedes',95)
